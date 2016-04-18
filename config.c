49c406b29312d4ee5facf652423d3e80b6e2e0b7
Trevor Drake <drake_trevor@projectara.com>
2016-04-18 01:04:23 +0100

    Initial Commit
    This is a fork of the oscam software [ http://www.streamboard.tv/oscam ]
    with a Android.mk for easy compiling with the rest of the platform.
    
    It Compiles. But Does it acutally work? That's the chance you take
    because as of this commit I haven't tested this with any hardware
    
    Good Luck.

---
+++ config.c
@@ -0,0 +1,14 @@
+const char *config_mak =
+  "\nCFG: strings FILE | sed -n 's/^CFG~//p' | openssl enc -d -base64 | gzip -d\n"
+  "CFG~H4sIACoIFFcCA32UwXLcIAyG732KzuQFms1sJj3kIAsZ08VAEbZ3c+ENeu/bF292\n"
+  "CFG~1qxFevL4+38JAUJP39G73ui8mDRkThDM+59vT5IeBUbvbct+4weZZ/r59kNQ+Oj8\n"
+  "CFG~WdARobCaWNNhvITk3//eOXWm3/0W30zW6z3+9XuieNlo8hMOlelaINuNDDBTVnO3\n"
+  "CFG~bvEOI4HKrFLGASJT4k0yUVHyWU/EbFy1PnIGlwwC4249Rd1UGUevJkvl40zycee1\n"
+  "CFG~3UNGBByIzhVbMl6whBeOp41bVDyF4GOqGEmG1uOpN1VGE+bXzXa/nluVCKN6ednu\n"
+  "CFG~qMZHsadPnBMGITlaVlWGYMEtyuXoSQh67aI9jKBIAychcCs3UzRg5ZLeccJF8ABO\n"
+  "CFG~+QiykiGlsLs9hKjW3qH42EsUswNdJ7nRz24SuPzBWdLru1h8PLHQmFBmnw0glj5t\n"
+  "CFG~CIq8nkq1QlL1od9Y8k73UDf7Tegmey1KCDqavjdOZtc7/3ZiOQye3LUz701YqcYl\n"
+  "CFG~ig6sHCQtU2No/cd22Pq75WI0zYIZ34x7bkpjqF9HHTNC1L4pqe6Afvxi+2Jk77Vj\n"
+  "CFG~cw+cyoy6dmMzsBSTvkgasMyxe8oylfNonLEfvs61YiB+9EV83XvKW35+NCmqunIF\n"
+  "CFG~pSNhH9YZ7aaxBP4D+xzNxL8GAAA=\n"
+  ;
